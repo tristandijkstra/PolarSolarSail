9978389b994964caae79286240b4dd76c6bd4c9d
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2023-03-05 22:36:06 +0100

    new intermediate sail model

--- plotQuiver.py
+++ plotQuiver.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 AU = 149.6e9
+yearInSeconds = 365 * 24 * 3600
 
 fig, ax = plt.subplots(1, 1, subplot_kw=dict(projection="3d"))
 
@@ -10,9 +11,13 @@ file = "SOLARSAILPropagationHistory_Q1.dat"
 file2 = "SOLARSAILPropagationHistory_DependentVariables_Q1.dat"
 file = "SOLARSAIL_Basic.dat"
 file2 = "SOLARSAIL_Dep_Basic.dat"
+file = "data/mass500_area45000.dat"
+file2 = "data/mass500_area45000_dep.dat"
+file = "data/mass500_area5625.0.dat"
+file2 = "data/mass500_area5625.0_dep.dat"
 
 data = pd.read_csv(
-    file, delimiter="	", names=["t", "x", "y", "z", "vx", "vy", "vz"], header=None
+    file, delimiter="	", names=["time", "x", "y", "z", "vx", "vy", "vz"], header=None
 ).assign(altitude=lambda x: np.sqrt(x.x**2 + x.y**2 + x.z**2) / AU)
 
 depVars = [
@@ -30,7 +35,7 @@ depVars = [
 ]
 data2 = pd.read_csv(file2, delimiter="	", names=depVars, header=None)
 
-
+time = (data2.time - data.time.iloc[0])/yearInSeconds
 ax.plot(data.iloc[:, 1], data.iloc[:, 2], data.iloc[:, 3])
 
 quiverEvery = 6000
@@ -45,24 +50,24 @@ ax.quiver(
     normalize=True,
     color="tab:orange",
 )
-ax.set_ylim(-AU, AU)
-ax.set_xlim(-AU, AU)
+ax.set_ylim(-0.5*AU, 0.5*AU)
+ax.set_xlim(-0.5*AU, 0.5*AU)
 # ax.set_zlim(-AU, AU)
 ax.set_aspect("equal")
 # fig.set_tight_layout()
 
-fig2, ax2 = plt.subplots(3, 1)
-ax2[0].plot(data2.iloc[:, 1:4], label=["x", "y", "z"])
-ax2[0].plot(data2.iloc[:, 4], label="norm", linestyle="--")
+fig2, ax2 = plt.subplots(3, 1, sharex=True)
+ax2[0].plot(time, data2.iloc[:, 1:4], label=["x", "y", "z"])
+ax2[0].plot(time, data2.iloc[:, 4], label="norm", linestyle="--")
 ax2[0].legend()
 ax2[0].set_title("Sail Acceleration")
 
 ax2[1].set_title("Altitude (AU)")
-ax2[1].plot(data.altitude)
+ax2[1].plot(time, data.altitude)
 ax2[2].set_title("Inclination (deg)")
-ax2[2].plot(np.degrees(data2.i))
+ax2[2].plot(time, np.degrees(data2.i))
 # fig2.set_tight_layout(True)
-
+ax2[2].set_xlabel("Time [years]")
 fig.set_tight_layout(True)
 fig2.set_tight_layout(True)
 
---
+++ solarsail/sailIntermediate.py
@@ -0,0 +1,119 @@
+from .base import SolarSailGuidanceBase
+
+
+import numpy as np
+
+from tudatpy.kernel import constants
+from tudatpy.kernel.numerical_simulation import environment
+from tudatpy.kernel.astro import element_conversion
+
+
+class SolarSailGuidance(SolarSailGuidanceBase):
+    def __init__(
+        self,
+        bodies: environment.SystemOfBodies,
+        sailName: str = "Sail",
+        mass: float = 100,
+        sailArea: float = 22500,
+        targetAltitude: float = 0.48,
+        # TODO: add fast transfer stuff
+        deepestAltitude: float = 0.48,
+        targetInclination: float = 90,
+        # Deprecated: here for backwards compatibility
+        maximum_thrust: float = 0,
+    ):
+        super().__init__(
+            bodies,
+            sailName,
+            mass,
+            sailArea,
+            targetAltitude,
+            deepestAltitude,
+            targetInclination,
+            maximum_thrust,
+        )
+
+    def computeSail(self, current_time) -> np.ndarray:
+        T = 100 + 273
+
+        current_cartesian_state = (
+            self.bodies.get(self.sailName).state - self.bodies.get("Sun").state
+        )
+        current_cartesian_velocity = (
+            self.bodies.get(self.sailName).velocity
+            # - self.bodies.get("Sun").velocity
+        )
+
+        current_cartesian_position = (
+            self.bodies.get(self.sailName).position - self.bodies.get("Sun").position
+        )
+
+        progradeDirection = current_cartesian_velocity / np.sqrt(
+            np.square(current_cartesian_velocity).sum()
+        )
+
+        current_alt = np.sqrt(np.square(current_cartesian_position).sum())
+
+        radialDirection = current_cartesian_position / np.sqrt(
+            np.square(current_cartesian_position).sum()
+        )
+
+        mu = self.bodies.get("Sun").gravitational_parameter
+
+        current_keplerian_state = element_conversion.cartesian_to_keplerian(
+            current_cartesian_state, mu
+        )
+        H = np.cross(current_cartesian_velocity, current_cartesian_position)
+        # Hnorm = np.cross(radialDirection, progradeDirection)
+        Hdirection = H / self.norm(H)
+
+        inclination = current_keplerian_state[2]
+
+        ###########################################
+        ############## Flight Phases ##############
+        ###########################################
+        # Transfer
+        if self.currentPhase == 0:
+            if current_alt / SolarSailGuidance.AU < self.targetAltitude:
+                print("Altitude reached -> Inclination Change")
+                self.inclinationChangeStart = current_time
+                self.currentPhase = 2
+            b = -progradeDirection
+
+        # wait for correct orbit
+        elif self.currentPhase == 1:
+            b = np.zeros([3, 1])
+
+        # inclination change
+        elif self.currentPhase == 2:
+            self.inclinationChangeEnd = current_time
+            self.lastInclination = np.degrees(inclination)
+            if inclination > self.targetInclination:
+                print("Inclination Change complete -> Science")
+                self.currentPhase = 3
+            
+            thetaNode = current_keplerian_state[3]
+            # thetaNode = (2 * np.pi) - current_keplerian_state[3]
+            trueAnomaly = current_keplerian_state[5]
+
+            nearNode1 = abs(thetaNode - trueAnomaly) < np.pi
+            nearNode2 = abs(thetaNode - np.pi - trueAnomaly) < np.pi
+            
+            if nearNode1:
+                b = Hdirection
+            elif nearNode2:
+                b = -Hdirection
+            else:
+                print(thetaNode - trueAnomaly)
+                b = Hdirection
+
+        # post-inclination change
+        elif self.currentPhase == 3:
+            b = np.zeros([3, 1])
+
+        else:
+            b = np.zeros([3, 1])
+
+        B = (mu / (current_alt * current_alt)) * (0.5 * (self.sigmaC / self.sigma))
+
+        return B * b
