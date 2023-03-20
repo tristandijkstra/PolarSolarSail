9e979a955bda019a7f37187a76e1d61ed40adb56
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2023-03-20 23:08:44 +0100

    test rk4

---
+++ runRK4Test.py
@@ -0,0 +1,190 @@
+from simulation import simulationV1 as sim
+from solarsail.sailPhysicalV2_2 import SolarSailGuidance
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import logging
+import os
+
+from itertools import product
+from tqdm import tqdm
+
+
+saveDirectory = "data"
+if not os.path.exists(saveDirectory):
+    os.mkdir(saveDirectory)
+
+logging.basicConfig(
+    filename=f"{saveDirectory}/simV0.log",
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s | %(message)s",
+)
+
+
+
+stepSize = 3600
+stepSize = 36000
+stepSizes = [360, 720, 3600, 7200, 36000, 72000,]
+stepSizes = [9000, 18000, 36000, 72000, 144000, 288000]
+# skipSizes = [int(x/min(stepSizes)) for x in stepSizes]
+skipSizes = [int(x/min(stepSizes)) for x in stepSizes][::-1]
+# stepSize = 18000
+
+paramNames = ["mass", "area"]
+
+yearsToRun = 25
+yearInSeconds = 365 * 24 * 3600
+targetAltitude = 0.48
+deepestAltitude = targetAltitude
+
+
+saveFiles = []
+resultslst = []
+
+for step in tqdm(stepSizes):
+    spacecraftName = f"rk4_{step}"
+
+    print(f"Running step: {step}")
+    guidanceObject = SolarSailGuidance(
+        None,
+        sailName=spacecraftName,
+        mass=500,
+        sailArea=10000,
+        targetAltitude=targetAltitude,
+        targetInclination=68,
+        deepestAltitude=deepestAltitude,
+        # fastTransferOptimiseParameter=0.04
+    )
+
+    finalGuidanceObj, save, saveDep = sim.simulate(
+        spacecraftName=spacecraftName,
+        sailGuidanceObject=guidanceObject,
+        saveFile=spacecraftName,
+        yearsToRun=yearsToRun,
+        simStepSize=step,
+    )
+
+    (
+        characteristicacceleration,
+        spiralDuration,
+        inclinationChangeDuration,
+        finalInclination,
+    ) = finalGuidanceObj.getInclinationChangeDuration()
+
+    dur = round(inclinationChangeDuration / yearInSeconds, 3)
+    totdur = round((spiralDuration + inclinationChangeDuration) / yearInSeconds, 3)
+    print(f"Final inclination = {round(finalInclination, 3)} deg")
+    print(f"Inclination change duration = {dur} years")
+
+    extraTxt = f"\nFinal inclin. = {round(finalInclination, 3)} | Characteristic Acceleration = {round(characteristicacceleration*1000, 4)} mm/s^2\
+                 \nInclin. change duration = {dur} years | Total duration = {totdur} years"
+    saveFiles.append([spacecraftName, save, saveDep, extraTxt])
+
+    resultslst.append(
+        [
+            step,
+            spiralDuration / yearInSeconds,
+            inclinationChangeDuration / yearInSeconds,
+        ]
+    )
+
+AU = 149.6e9  # m
+yearInSeconds = 365 * 24 * 3600
+
+fig, ax = plt.subplots(3, 1)
+fig3, ax3 = plt.subplots(1, 1, subplot_kw=dict(projection="3d"))
+cols = ["time", "x", "y", "z", "vx", "vy", "vz"]
+depVars = [
+    "time",
+    "ThrustX",
+    "ThrustY",
+    "ThrustZ",
+    "ThrustMagnitude",
+    "a",
+    "e",
+    "i",
+    "omega",
+    "RAAN",
+    "theta",
+    "cone",
+    "clock",
+]
+
+spacecraftName, save, saveDep, extraTxt = saveFiles[0]
+datasmallest = pd.read_csv(
+    save,
+    delimiter="	",
+    names=cols,
+    header=None,
+    skiprows=lambda i: i % skipSizes[0],
+    ).iloc[0:520]
+for idx, u in enumerate(saveFiles):
+
+    spacecraftName, save, saveDep, extraTxt = u
+    data = pd.read_csv(
+        save,
+        delimiter="	",
+        names=cols,
+        header=None,
+        skiprows=lambda i: i % skipSizes[idx],
+    )
+    ax[0].plot(data.time.iloc[0:520], (data.x - datasmallest.x).iloc[0:520].abs(), label=spacecraftName)
+    ax[1].plot(data.time.iloc[0:520], (data.y - datasmallest.y).iloc[0:520].abs(), label=spacecraftName)
+    ax[2].plot(data.time.iloc[0:520], (data.z - datasmallest.z).iloc[0:520].abs(), label=spacecraftName)
+
+    ax3.plot(data.x, data.y, data.z)
+    print(len(data))
+
+print(skipSizes)
+
+ax[0].grid()
+ax[1].grid()
+ax[2].grid()
+ax[0].legend()
+ax[1].legend()
+ax[2].legend()
+ax[0].set_yscale("log")
+ax[1].set_yscale("log")
+ax[2].set_yscale("log")
+fig.set_tight_layout(True)
+
+fig2, ax2 = plt.subplots(1,1)
+print(np.array(resultslst)[:,0])
+print(np.array(resultslst)[:,2])
+ax2.bar(x=[str(x) for x in stepSizes], height=np.array(resultslst)[:,2]-np.array(resultslst)[0,2])
+plt.show()
+
+# things = np.array(resultslst)
+# levelss = 100
+
+# fig, ax = plt.subplots(1,1)
+# ax.set_title("Spiral Transfer Time")
+# ax.set_ylabel("Sail Area [m^2]")
+# ax.set_xlabel("Spacecraft Mass [kg]")
+# im = ax.tricontour(things[:, 0], things[:, 1], things[:, 2], levels=levelss)
+# cbar = plt.colorbar(im, )
+# cbar.set_label("Spiral Transfer Time [years]")
+# fig.savefig("doc/spiraltime2.png")
+
+
+# fig2, ax2 = plt.subplots(1,1)
+# ax2.set_title("Inclination Change Time")
+# ax2.set_ylabel("Sail Area [m^2]")
+# ax2.set_xlabel("Spacecraft Mass [kg]")
+# im2 = ax2.tricontour(things[:, 0], things[:, 1], things[:, 3], levels=levelss)
+# cbar2 = plt.colorbar(im2, )
+# cbar2.set_label("Inclination Change Time [years]")
+# fig2.savefig("doc/inclinationtime2.png")
+
+
+# fig3, ax3 = plt.subplots(1,1)
+# ax3.set_title("Total Transfer Time")
+# ax3.set_ylabel("Sail Area [m^2]")
+# ax3.set_xlabel("Spacecraft Mass [kg]")
+# im3 = ax3.tricontour(things[:, 0], things[:, 1], things[:, 2] + things[:, 3], levels=levelss)
+# cbar3 = plt.colorbar(im3, )
+# cbar3.set_label("Total Transfer Time [years]")
+# fig3.savefig("doc/totaltransfertime2.png")
+
+# plt.show()
---
+++ solarsail/sailPhysicalV2_2.py
@@ -0,0 +1,174 @@
+from .base import SolarSailGuidanceBase
+
+
+import numpy as np
+
+from tudatpy.kernel import constants
+from tudatpy.kernel.numerical_simulation import environment
+from tudatpy.kernel.astro import element_conversion
+from scipy.optimize import fsolve
+
+from typing import Union
+
+from .transform import simplifiedSailToInertial
+
+
+def lambdFunc(alpha, lamd=0):
+    return (2 - 4 * np.tan(alpha) ** 2) / (
+        np.cos(alpha) * (2 - np.tan(alpha) ** 2)
+    ) - lamd
+
+
+class SolarSailGuidance(SolarSailGuidanceBase):
+    def __init__(
+        self,
+        bodies: environment.SystemOfBodies,
+        sailName: str = "Sail",
+        mass: float = 100,
+        sailArea: float = 22500,
+        targetAltitude: float = 0.48,
+        # TODO: add fast transfer stuff
+        deepestAltitude: float = 0.48,
+        targetInclination: float = 90,
+        characteristicAcceleration: Union[None, float] = None,
+    ):
+        super().__init__(
+            bodies,
+            sailName,
+            mass,
+            sailArea,
+            targetAltitude,
+            deepestAltitude,
+            targetInclination,
+            characteristicAcceleration,
+        )
+
+        lambd = 168.6284 * self.charAccel
+        self.spiralAlpha = fsolve(lambdFunc, 30 * np.pi / 180, args=(lambd))[0]
+        self.inclinationChangeAlpha = np.arctan(1 / np.sqrt(2))
+        
+        print("=== Cone angles ===")
+        print(f"Spiral Alpha = {round(self.spiralAlpha, 4)} rad = {round(np.degrees(self.spiralAlpha), 4)} deg")
+        print(f"Inclination Change Alpha = {round(self.inclinationChangeAlpha, 4)} rad = {round(np.degrees(self.inclinationChangeAlpha), 4)} deg")
+        
+        self.currentPhase:int = -1
+        self.startTime = 0
+
+        
+    def stopPropagation(self, time):
+        if self.currentPhase == 3:
+            return True
+        else:
+            return False
+        
+    def getInclinationChangeDuration(self):
+        inclinationChangeDuration = (
+            self.inclinationChangeEnd - self.inclinationChangeStart
+        )
+        spiralDuration = (
+            self.inclinationChangeStart - self.startTime
+        )
+        return self.charAccel, spiralDuration, inclinationChangeDuration, self.lastInclination
+
+
+    def computeSail(self, current_time) -> np.ndarray:
+        current_cartesian_state = (
+            self.bodies.get(self.sailName).state - self.bodies.get("Sun").state
+        )
+        # current_cartesian_velocity = (
+        #     self.bodies.get(self.sailName).velocity
+        #     # - self.bodies.get("Sun").velocity
+        # )
+
+        current_cartesian_position = (
+            self.bodies.get(self.sailName).position - self.bodies.get("Sun").position
+        )
+
+        # progradeDirection = current_cartesian_velocity / np.sqrt(
+        #     np.square(current_cartesian_velocity).sum()
+        # )
+
+        current_alt = np.sqrt(np.square(current_cartesian_position).sum())
+
+        # radialDirection = current_cartesian_position / np.sqrt(
+        #     np.square(current_cartesian_position).sum()
+        # )
+
+        mu = self.bodies.get("Sun").gravitational_parameter
+
+        current_keplerian_state = element_conversion.cartesian_to_keplerian(
+            current_cartesian_state, mu
+        )
+        # H = np.cross(current_cartesian_velocity, current_cartesian_position)
+        # Hdirection = H / self.norm(H)
+
+        inclination = current_keplerian_state[2]
+
+        trueAnomaly = current_keplerian_state[5]
+        argPeriapsis = current_keplerian_state[3]
+        RAAN = current_keplerian_state[4]
+
+        ###########################################
+        ############## Flight Phases ##############
+        ###########################################
+        if self.currentPhase == -1:
+            self.startTime = current_time
+            self.currentPhase = 0
+        # Transfer
+        if self.currentPhase == 0:
+            if current_alt / SolarSailGuidance.AU < self.targetAltitude:
+                print("Altitude reached -> Inclination Change")
+                self.inclinationChangeStart = current_time
+                self.currentPhase = 2
+
+            self.delta = 1.5 * np.pi
+            self.alpha = self.spiralAlpha
+
+        # wait for correct orbit
+        elif self.currentPhase == 1:
+            # TODO
+            self.alpha = 0
+            self.delta = 0
+
+        # inclination change
+        elif self.currentPhase == 2:
+            self.inclinationChangeEnd = current_time
+            self.lastInclination = np.degrees(inclination)
+
+            if inclination > self.targetInclination:
+                print("Inclination Change complete -> Science")
+                self.currentPhase = 3
+
+            self.alpha = self.inclinationChangeAlpha
+
+            if np.cos(argPeriapsis + trueAnomaly) >= 0:
+                self.delta = 0
+            else:
+                self.delta = np.pi
+
+        # post-inclination change
+        elif self.currentPhase == 3:
+            # TODO
+            self.alpha = 0
+            self.delta = 0
+        else:
+            self.alpha = 0
+            self.delta = 0
+
+        # B = (mu / (current_alt * current_alt)) * (0.5 * (self.sigmaC / self.sigma))
+
+        nVecAlt = np.array(
+            [
+                np.cos(self.alpha),
+                np.sin(self.alpha) * np.sin(self.delta),
+                np.sin(self.alpha) * np.cos(self.delta),
+            ]
+        )
+
+        F0 = (SolarSailGuidanceBase.AU / current_alt) ** 2 * self.charAccel
+
+        ForceRTN = F0 * (np.cos(self.alpha) ** 2) * nVecAlt
+
+        b = simplifiedSailToInertial(inclination, argPeriapsis, trueAnomaly, RAAN) @ ForceRTN
+
+        return b
