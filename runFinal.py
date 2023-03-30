fedb3f354f1933fbe9a42ce90429aa1a1d0c6f3e
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2023-03-30 16:53:25 +0200

    some changes for thermal

--- runFinal.py
+++ runFinal.py
@@ -8,7 +8,7 @@ import os
 
 from itertools import product
 from tqdm import tqdm
-from thermal.thermal_model import Thermal
+
 
 saveDirectory = "data"
 if not os.path.exists(saveDirectory):
@@ -23,22 +23,32 @@ logging.basicConfig(
 
 
 masses = [760]
+# sailAreas = [6000, 6500, 7000, 7500, 8000, 9000, 9500]
 sailAreas = [10000]
+# sailAreas = [120]
+# sailAreas = [7000,10000]
+sailAreas = sailAreas[::-1]
 
 stepSize = 3600
-# stepSize = 100000
+stepSize = 14400
+# stepSize = 18000
 
 paramNames = ["mass", "area"]
 
-yearsToRun = 25
+yearsToRun = 50
 yearInSeconds = 365 * 24 * 3600
 targetAltitude = 0.48
 deepestAltitude = 0.4
+FTOP = 0.05727209780778128
+initialEpoch = 1117800000
+escapeBurnDV = np.array([0.084108927717811,0.240321913434112,4.946831358585164]) * 1000
+targetInclination = 52.75
 
 combinations = list(product(masses, sailAreas))
 
 saveFiles = []
 resultslst = []
+finalEpoch = 0
 
 logging.info(f"=== Starting run with {len(combinations)} combinations ===")
 for combination in tqdm(combinations):
@@ -51,10 +61,9 @@ for combination in tqdm(combinations):
         mass=combination[0],
         sailArea=combination[1],
         targetAltitude=targetAltitude,
-        targetInclination=68,
+        targetInclination=targetInclination,
         deepestAltitude=deepestAltitude,
-        fastTransferOptimiseParameter=0.04,
-        thermalModel=Thermal()
+        fastTransferOptimiseParameter=FTOP
     )
 
     finalGuidanceObj, save, saveDep = sim.simulate(
@@ -63,6 +72,8 @@ for combination in tqdm(combinations):
         saveFile=spacecraftName,
         yearsToRun=yearsToRun,
         simStepSize=stepSize,
+        initialEpoch=initialEpoch,
+        C3BurnVector=escapeBurnDV
     )
 
     (
@@ -71,6 +82,9 @@ for combination in tqdm(combinations):
         inclinationChangeDuration,
         finalInclination,
     ) = finalGuidanceObj.getInclinationChangeDuration()
+
+    finalEpoch = 1117886400 + spiralDuration + inclinationChangeDuration
+
     dur = round(inclinationChangeDuration / yearInSeconds, 3)
     totdur = round((spiralDuration + inclinationChangeDuration) / yearInSeconds, 3)
     print(f"Final inclination = {round(finalInclination, 3)} deg")
@@ -78,7 +92,7 @@ for combination in tqdm(combinations):
 
     extraTxt = f"\nFinal inclin. = {round(finalInclination, 3)} | Characteristic Acceleration = {round(characteristicacceleration*1000, 4)} mm/s^2\
                  \nInclin. change duration = {dur} years | Total duration = {totdur} years"
-    saveFiles.append([spacecraftName, save, saveDep, extraTxt, finalGuidanceObj.thermalModel.node_keys])
+    saveFiles.append([spacecraftName, save, saveDep, extraTxt])
 
     logStr = f"run: {spacecraftName} | mass = {combination[0]} | area = {combination[1]} | Final inclin. = {round(finalInclination, 3)} | Inclin. change duration = {dur} years"
     logging.info(logStr)
@@ -93,8 +107,12 @@ for combination in tqdm(combinations):
     )
 
 for u in saveFiles:
-    sim.plotSimulation(*u[0:4], quiverEvery=1000, thermalOn=True)
-    sim.plotThermal(u[0], u[2], u[4])
+    sim.plotSimulation(*u, quiverEvery=0)
 
+planets = ["Earth", "Venus", "Mercury"]
+
+# for planet in planets:
+#     print(f"Running {planet}")
+#     sim.simulatePlanet(planet, 1117886400, finalEpoch, stepSize)
 
 plt.show()
