69e4036f2a0390cdd04c5e05ac5743fcb10e8681
Tristan Dijkstra <tristan.dijkstra@gmail.com>
2023-03-31 21:55:09 +0200

    updates to the configs

--- runFinal.py
+++ runFinal.py
@@ -22,19 +22,19 @@ logging.basicConfig(
 )
 
 
-masses = [500]
+masses = [590]
 # masses = [660, 760]
 # masses = [500]
 # masses = [660]
 # sailAreas = [6000, 6500, 7000, 7500, 8000, 9000, 9500]
-sailAreas = [6400]
+sailAreas = [10000]
 # sailAreas = [5000]
 # sailAreas = [120]
 # sailAreas = [7000,10000]
 sailAreas = sailAreas[::-1]
 
 stepSize = 3600
-stepSize = 14400
+# stepSize = 14400
 # stepSize = 18000
 
 paramNames = ["mass", "area"]
--- runSimV1Thermal.py
+++ runSimV1Thermal.py
@@ -22,7 +22,7 @@ logging.basicConfig(
 )
 
 
-masses = [605]
+masses = [590]
 sailAreas = [10000]
 
 stepSize = 36000
@@ -34,7 +34,7 @@ paramNames = ["mass", "area"]
 yearsToRun = 25
 yearInSeconds = 365 * 24 * 3600
 targetAltitude = 0.48
-deepestAltitude = 0.40
+deepestAltitude = 0.45
 
 combinations = list(product(masses, sailAreas))
 
@@ -55,7 +55,7 @@ for combination in tqdm(combinations):
         targetInclination=68,
         deepestAltitude=deepestAltitude,
         fastTransferOptimiseParameter=0.04,
-        thermalModel=Thermal(stepSize)
+        thermalModel=Thermal(100*3600)
     )
 
     finalGuidanceObj, save, saveDep = sim.simulate(
